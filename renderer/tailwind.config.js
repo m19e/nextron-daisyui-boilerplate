cdeff89ca6c24e3c2ace2e2be11c63bb44cd7754
m19e <nexus.me7290@gmail.com>
2022-08-21 20:37:43 +0900

    Add tailwind-typography, daisy-ui plugins

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -1,18 +1,15 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: [
-    './renderer/pages/**/*.{js,ts,jsx,tsx}',
-    './renderer/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    colors: {
-      // use colors only specified
-      white: colors.white,
-      gray: colors.gray,
-      blue: colors.blue,
+    content: ["./renderer/pages/**/*.{js,ts,jsx,tsx}", "./renderer/components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        colors: {
+            // use colors only specified
+            white: colors.white,
+            gray: colors.gray,
+            blue: colors.blue,
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
